96f0daf83ed7c82fbab0b3b74311336e4d08dc7e
Neftalem <dneftalem@gmail.com>
2025-08-27 14:47:57 +0300

    finshed quations

--- app.py
+++ app.py
@@ -6,3 +6,24 @@ questions = {
     "Which planet is known as the Red Planet?": "Mars",
     "What does HTML stand for?": "HyperText Markup Language"
 }
+
+def run_questions(quiz_question):
+    score = 0
+    print('Welcome')
+
+    for question, answer in quiz_question.items():
+        print(question)
+
+        user_answer = input('Enter the answer: ')
+
+        if answer == user_answer:
+            print('correct')
+            score += 1
+
+        else:
+            print(f'not correct the answer is {answer}')
+
+    print(f'score {score}/ {len(quiz_question)}')
+
+if __name__ == "__main__":
+    run_questions(questions)
