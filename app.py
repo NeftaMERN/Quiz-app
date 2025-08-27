121cc3fa97accad2e3b95a9c5eb9de09d7155043
Neftalem <dneftalem@gmail.com>
2025-08-27 14:38:08 +0300

    add quations

---
+++ app.py
@@ -0,0 +1,8 @@
+
+questions = {
+    "What is the capital of France?": "Paris",
+    "Who developed Python Programming Language?": "Guido van Rossum",
+    "What is 5 + 7?": "12",
+    "Which planet is known as the Red Planet?": "Mars",
+    "What does HTML stand for?": "HyperText Markup Language"
+}
